24ef6ccc43cb4b52a31363fc68fa5ed1ed1e92d8
Andrea993 <andrea69x@hotmail.it>
2016-10-14 12:51:54 +0200

    update tmux and fish

--- fish/config.fish
+++ fish/config.fish
@@ -1,10 +1,11 @@
+#!/usr/bin/fish
 set PATH $PATH ~/bin /home/andrearch/.gem/ruby/2.3.0/bin
 set EDITOR vim
 set SYSROOT ~/Android/ndkTC/sysroot
 set CC "arm-linux-androidrabi-gcc --sysroot $SYSROOT"
 set AR arm-linux-androideabi-ar
 
-
+alias tmux "tmux -2"
 alias vundle-update "env SHELL=(which sh) vim +BundleInstall! +BundleClean +qall"
 
 function pythonversion 
